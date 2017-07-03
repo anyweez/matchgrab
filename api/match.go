902a420f2749d60e132019ee1ce8be5c3ed6c1b1
Luke Segars <luke@lukesegars.com>
2017-07-02 23:59:05 -0400

    Request and receive gzipped data (no api changes!)

--- api/match.go
+++ api/match.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"compress/gzip"
 	"io/ioutil"
 	"net/http"
 
@@ -20,6 +21,7 @@ func Get(url string, cb func(body []byte)) error {
 		return err
 	}
 	request.Header.Set("X-Riot-Token", config.Config.RiotAPIKey)
+	request.Header.Set("Accept-Encoding", "gzip")
 
 	resp, err := client.Do(request)
 	if err != nil {
@@ -27,7 +29,14 @@ func Get(url string, cb func(body []byte)) error {
 	}
 
 	defer resp.Body.Close()
-	raw, err := ioutil.ReadAll(resp.Body)
+
+	// Decode the body first if its gzip'd.
+	src := resp.Body
+	if resp.Header.Get("Content-Encoding") == "gzip" {
+		src, _ = gzip.NewReader(src)
+	}
+
+	raw, err := ioutil.ReadAll(src)
 	if err != nil {
 		return err
 	}
